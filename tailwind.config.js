f4f3dcd7cc0c34ec716424ba2ef2bdf5766b2fb4
QuangNV <quangnvq1998@gmail.com>
2022-10-03 23:06:16 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    spacing: {
+      '20': '20px',
+      '25': '25px',
+      '26': '26px',
+      '45': '45px',
+      '58': '58px',
+      '98': '98px'
+    },
+    colors: {
+      'black-07': 'rgba(0,0,0,.7) 10%',
+      'white': '#FFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
